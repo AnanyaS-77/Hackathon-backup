b67083e2019fbfd038c4a20f4b0a1608c9e77f66
Ananya Singh <23btrca012@jainuniversity.ac.in>
2026-04-09 11:25:27 +0530

    fixed port

--- app.py
+++ app.py
@@ -64,4 +64,5 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5003, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
